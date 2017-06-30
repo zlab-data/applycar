87784bbbf360c882f2d39a77c04d9a70d84c0b74
wjy4528 <wjy4528@gmail.com>
2017-06-30 10:40:48 +0800

    swith db

--- server/main.js
+++ server/main.js
@@ -3,9 +3,9 @@ import { Meteor } from 'meteor/meteor';
 Meteor.startup(() => {
   // code to run on server at startup
  	Cloudinary.config({
- 		cloud_name:'dcyqpeqbh',
- 		api_key:'448942765988189',
- 		api_secret: 'twiBkq48MujSH08yJ_HEli3SxhI',
+ 		cloud_name:'zlabdata',
+ 		api_key:'287546948217581',
+ 		api_secret: 'dbq5a9wfGLooe1IKx57AMj0DFW0',
 
  	}),
  	Accounts.config({

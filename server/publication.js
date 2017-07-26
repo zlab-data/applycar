c33b5a8da603b7370a8ef69f19ca70725f8943a8
Mark Wang <wangyouan0629@hotmail.com>
2017-07-26 10:36:49 +0800

    add new account

--- server/publication.js
+++ server/publication.js
@@ -1,40 +1,51 @@
-Meteor.publish('posts', function() {
-  return Posts.find();
+Meteor.publish('posts', function () {
+    return Posts.find();
 });
 
 
-Meteor.publish('records', function() {
-  return Records.find();
+Meteor.publish('records', function () {
+    return Records.find();
 });
 
 //console.log(Meteor.users.findOne({username: 'zlabtest'}));
 
-if (typeof Meteor.users.findOne({username: 'zlabtest'}) === "undefined" ){
-  Accounts.createUser({
-                           username: 'zlabtest',
-                           password : 'iamadmin123',
-                           profile  : {
-                                                                           //publicly visible fields like firstname goes here
-                        }
-  });
-};
+if (typeof Meteor.users.findOne({username: 'zlabtest'}) === "undefined") {
+    Accounts.createUser({
+        username: 'zlabtest',
+        password: 'iamadmin123',
+        profile: {
+            //publicly visible fields like firstname goes here
+        }
+    });
+}
 
-if (typeof Meteor.users.findOne({username: 'daiyongfeng'}) === "undefined" ){
-  Accounts.createUser({
-                           username: 'daiyongfeng',
-                           password : 'zhishu123',
-                           profile  : {
-                                                                           //publicly visible fields like firstname goes here
-                        }
-  });
-};
+if (typeof Meteor.users.findOne({username: 'daiyongfeng'}) === "undefined") {
+    Accounts.createUser({
+        username: 'daiyongfeng',
+        password: 'zhishu123',
+        profile: {
+            //publicly visible fields like firstname goes here
+        }
+    });
+}
 
-if (typeof Meteor.users.findOne({username: '君胜汽车金融'}) === "undefined" ){
-  Accounts.createUser({
-                           username: '君胜汽车金融',
-                           password : 'junsheng123',
-                           profile  : {
-                                                                           //publicly visible fields like firstname goes here
-                        }
-  });
+
+if (typeof Meteor.users.findOne({username: '君胜汽车金融'}) === "undefined") {
+    Accounts.createUser({
+        username: '君胜汽车金融',
+        password: 'junsheng123',
+        profile: {
+            //publicly visible fields like firstname goes here
+        }
+    });
+}
+
+if (typeof Meteor.users.findOne({username: '永旺汽车金融'}) === "undefined") {
+    Accounts.createUser({
+        username: '永旺汽车金融',
+        password: 'yongwang123',
+        profile: {
+            //publicly visible fields like firstname goes here
+        }
+    });
 }

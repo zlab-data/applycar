ed9567bd65e05821eb91dd8acd94ef370c6ea7d6
Mark Wang <wangyouan0629@hotmail.com>
2017-08-19 18:08:16 +0800

    Optimize login page

--- client/templates/includes/login_page.js
+++ client/templates/includes/login_page.js
@@ -9,14 +9,13 @@ Template.login.events({
             if (result) {
                 Meteor.loginWithPassword(username, password, function (error) {
                     if (error) {
-                        alert('密码错误');
+                        alert('用户名密码不匹配');
                     } else {
-                        alert('登陆成功');
                         Router.go('/confirm')
                     }
                 });
             } else {
-                alert('用户名错误')
+                alert('用户名密码不匹配')
             }
         });
     }
